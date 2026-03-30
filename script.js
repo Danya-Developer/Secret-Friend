5daf1a986c1276bc257940fed7b1d93dcb007c3b
Danya-Developer <sahovdaniil58@gmail.com>
2026-03-30 13:17:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'https://your-server-api.com/api'; // тут короче должна быть ссылка на сервер(сотри этот комментарий)
+    const API_BASE_URL = 'https://your-server-api.com/api';
     
     const joinSection = document.querySelector('.join-section') || document.body; 
     const nameInput = document.getElementById('user-name');
