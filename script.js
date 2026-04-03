2829e896634d118759eb04afd3001479710acfaa
Danya-Developer <sahovdaniil58@gmail.com>
2026-04-03 17:11:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'https://api/santa';
+    const API_BASE_URL = '/api/santa';
     
     const joinSection = document.querySelector('.join-section');
     const userNameInput = document.getElementById('user-name');
